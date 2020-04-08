8ca7605c4f8201681bfdb2e7d8d694e1023bed77
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2020-04-08 15:09:34 +0300

    [alias] Adding force authenticate to Yubikey helper function/alias.

--- .zsh.d/amzn/core.zsh
+++ .zsh.d/amzn/core.zsh
@@ -3,9 +3,37 @@ alias SHUTUP='export MAKE_OUTPUT_LEVEL=QUIET'
 ############################## authentication ###################################
 function j-yubikey {
   # https://w.amazon.com/index.php/NextGenMidway/UserGuide/mwinit/Advanced_Daily_Setup_Process
-  SSH_CERT=~/.ssh/id_rsa-cert.pub
+  local USAGE="Usage: ${0} [force]. The 'force' argument is optional."
+
+  if [[ ${#} -gt  1 ]]; then
+    echo "Wrong number of arguments"
+    echo ${USAGE}
+    return
+  fi
+
+  local SSH_CERT=~/.ssh/id_rsa-cert.pub
+  local EXPIRED=false
+  local FORCE_AUTH=false
   if (! test -f "$SSH_CERT") || (test "`find ~/.ssh/id_rsa-cert.pub -mmin +1220`"); then
+    EXPIRED=true
     echo "Midway expired"
+  else
+    echo "Midway has not expired"
+  fi
+
+  if [[ ${#} -eq 1 ]]; then
+    if [[ ${1} == "force" ]]; then
+      # Force authentication
+      echo "Forcing Midway authentication"
+      FORCE_AUTH=true
+    else
+      echo "'${1}': unsupported argument"
+      echo ${USAGE}
+      return
+    fi
+  fi
+
+  if [[ "${EXPIRED}" = true || "${FORCE_AUTH}" = true ]]; then
     if mwinit; then
       ssh-add -D ~/.ssh/*_rsa
       ssh-add ~/.ssh/*_rsa
@@ -13,6 +41,9 @@ function j-yubikey {
       echo "Failed to authenticate."
       exit 1
     fi
+  else
+    echo "Nothing to do."
+    return
   fi
 }
 
