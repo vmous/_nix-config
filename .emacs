67c9b61b5297cb62234e360797649080bbd6478d
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2024-03-14 11:37:43 +0100

    [emacs] Redefining custom face for `window-numbering` package
    Custom face definition for `window-numbering` was defined in both
    Custom's auto-generated `custom-set-faces` function and in
    `window-numbering`'s `:config` tag. Utilizing `use-package`'s
    `:custom-faces` conf to configure `window-numbering`'s face in one
    place and making sure there is only one definition of
    `custom-set-faces` in the Emacs' init files.

--- .emacs
+++ .emacs
@@ -174,7 +174,7 @@ This command does not push text to `kill-ring'."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
+)
 
 
 ;;;; package
--- .emacs.d/core.el
+++ .emacs.d/core.el
@@ -22,11 +22,11 @@
 ;; https://github.com/nschum/window-numbering.el
 (use-package window-numbering
   :ensure t
-  :config
+  :custom-face
   ;; highlight the window number in pink color
-  (custom-set-faces '(window-numbering-face
-                      ((t(:foreground "DeepPink" :underline "DeepPink" :weight bold)))))
-  (window-numbering-mode))
+  (window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold)) ))
+  :init
+  (window-numbering-mode 1))
 
 
 ;;;;;; neotree
