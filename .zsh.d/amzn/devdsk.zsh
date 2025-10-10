2b7f6f17abec6345a8bd8c6ccf895a98e520b3a7
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2025-10-10 13:18:33 +0000

    [zsh] Setting-up Amazon Q CLI integration.

--- .zsh.d/amzn/devdsk.zsh
+++ .zsh.d/amzn/devdsk.zsh
@@ -5,6 +5,9 @@
 # source /home/$USER/.zshrc_amzn
 #
 
+# Amazon Q pre block. Keep at the top of this file.
+[[ -f "${HOME}/.local/share/amazon-q/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/.local/share/amazon-q/shell/zshrc.pre.zsh"
+
 # Interesting configuration at sourced by the following
 # /apollo/env/EnvImprovement/dotfiles/zshrc
 # /apollo/env/EnvImprovement/dotfiles/anyshrc
@@ -32,3 +35,7 @@ alias j-perm-sync='sudo /usr/multipass/bin/auth-sync.pl -a'
 
 # alternative: /apollo/bin/env -e SDETools /apollo/env/SDETools/bin/curl --url %s -k --negotiate -u :
 alias curl_sdetools='/apollo/bin/env -e SDETools /apollo/env/SDETools/bin/curl --anyauth --location-trusted -u: -c /tmp/cookies.txt -b /tmp/cookies.txt -k'
+
+# Amazon Q post block. Keep at the bottom of this file.
+[[ -f "${HOME}/.local/share/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/.local/share/amazon-q/shell/zshrc.po\
+st.zsh"
