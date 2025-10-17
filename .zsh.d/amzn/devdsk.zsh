177bcae53a89f2b08a48b8b36bed81904833e833
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2025-10-17 13:51:20 +0200

    [zsh] Defining `amzn-mcp`'s environment configuration for DevDesktop.

--- .zsh.d/amzn/devdsk.zsh
+++ .zsh.d/amzn/devdsk.zsh
@@ -36,6 +36,14 @@ alias j-perm-sync='sudo /usr/multipass/bin/auth-sync.pl -a'
 # alternative: /apollo/bin/env -e SDETools /apollo/env/SDETools/bin/curl --url %s -k --negotiate -u :
 alias curl_sdetools='/apollo/bin/env -e SDETools /apollo/env/SDETools/bin/curl --anyauth --location-trusted -u: -c /tmp/cookies.txt -b /tmp/cookies.txt -k'
 
+#### Amazon-Internal-MCP-Server
+### https://w.amazon.com/bin/view/Amazon-Internal-MCP-Server/
+# E.g., Quip configuration setup:
+# - Go to https://quip-amazon.com/dev/token and generate your personal token
+# - Add the `QUIP_API_TOKEN=<PERSONAL_QUIP_TOKEN>` to the Amazon Internal MCP
+#   Server defined below (create it if it does not exist yet)
+AMAZON_MCP_SERVER_CONFIG_PATH=${HOME}/.amazon-internal-mcp-server/.env
+
 # Amazon Q post block. Keep at the bottom of this file.
 [[ -f "${HOME}/.local/share/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/.local/share/amazon-q/shell/zshrc.po\
 st.zsh"
